00f0d70f4b17a7c73c62f255070fb4a9e7f82375
Vicente Lubascher <velubascher@uc.cl>
2020-05-04 23:26:25 -0400

    Updates

---
+++ precache-manifest.cf2bfcbb6f8983bd2bad9789916284cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c752aafc7efefb01c6a1688a1b63c44",
+    "url": "/stonks/index.html"
+  },
+  {
+    "revision": "753c7d331081e8a5d5f8",
+    "url": "/stonks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "502a3d815fbd5acee387",
+    "url": "/stonks/static/js/2.ff4b9d49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stonks/static/js/2.ff4b9d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753c7d331081e8a5d5f8",
+    "url": "/stonks/static/js/main.92b6e5c4.chunk.js"
+  },
+  {
+    "revision": "71e93e309693974e0dc6",
+    "url": "/stonks/static/js/runtime-main.2a272e5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stonks/static/media/logo.5d5d9eef.svg"
+  }
+]);

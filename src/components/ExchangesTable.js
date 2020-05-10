bf5c9a72c0a9cdbcdfa3867460e10548851fcc65
Vicente Lubascher <velubascher@uc.cl>
2020-05-10 19:22:53 -0400

    market share

--- src/components/ExchangesTable.js
+++ src/components/ExchangesTable.js
@@ -19,51 +19,68 @@ function getTickers(exchange, exchanges, stocks){
   return tickers.filter(ticker => ticker);
 }
 
-function ExchangesTable(props){
-    const { exchange, exchanges, stocks, buy, sell, update } = props;
-    let exchangeStocks;
-    if (exchange && exchanges && stocks){
-      exchangeStocks = getTickers(exchange, exchanges, stocks);
-    }
+function marketShare(exchange, exchanges, stocks, buy, sell, volume){
+  let total = 0;
+  let tickers = [];
+  let exchanges_list = Object.keys(exchanges);
 
-    const buyVolume = (exchangeStocks && tradeVolume(exchangeStocks, buy)) || 0;
-    const sellVolume = (exchangeStocks && tradeVolume(exchangeStocks, sell)) || 0;
-    
-    return (
-      <Table aria-label="simple table">
-        <TableRow key='volumen compra'>
-          <TableCell component="th" scope="row">
-            Volumen Compra
-          </TableCell>
-          <TableCell align="right">{buyVolume}</TableCell>
-        </TableRow>
-        <TableRow key='volumen venta'>
-          <TableCell component="th" scope="row">
-            Volumen Venta
-          </TableCell>
-          <TableCell align="right">{sellVolume}</TableCell>
-        </TableRow>
-        <TableRow key='volumen total'>
-          <TableCell component="th" scope="row">
-            Volumen Total
-          </TableCell>
-          <TableCell align="right">{sellVolume + buyVolume}</TableCell>
-        </TableRow>
-        <TableRow key='cantidad acciones'>
-          <TableCell component="th" scope="row">
-            Cantidad de Acciones
-          </TableCell>
-          <TableCell align="right">0</TableCell>
-        </TableRow>
-        <TableRow key='participacion de mercado'>
-          <TableCell component="th" scope="row">
-            Participación de Mercado
-          </TableCell>
-          <TableCell align="right">0</TableCell>
-        </TableRow>
-      </Table>
-      )
+  exchanges_list.forEach((current_exchange) => {
+    if (current_exchange !== exchange) {
+      tickers = getTickers(current_exchange, exchanges, stocks);
+      total += tradeVolume(tickers, buy) + tradeVolume(tickers, sell);
+      }
+  })
+  console.log('volume', volume)
+  console.log('total', total)
+  let market_share = volume/(volume + total) * 100;
+  return market_share.toFixed(4);
+}
+
+function ExchangesTable(props){
+  const { exchange, exchanges, stocks, buy, sell } = props;
+  let exchangeStocks;
+  if (exchange && exchanges && stocks){
+    exchangeStocks = getTickers(exchange, exchanges, stocks);
   }
+
+  const buyVolume = (exchangeStocks && tradeVolume(exchangeStocks, buy)) || 0;
+  const sellVolume = (exchangeStocks && tradeVolume(exchangeStocks, sell)) || 0;
+
+  return (
+    <Table aria-label="simple table">
+      <TableRow key='volumen compra'>
+        <TableCell component="th" scope="row">
+          Volumen Compra
+        </TableCell>
+        <TableCell align="right">{buyVolume}</TableCell>
+      </TableRow>
+      <TableRow key='volumen venta'>
+        <TableCell component="th" scope="row">
+          Volumen Venta
+        </TableCell>
+        <TableCell align="right">{sellVolume}</TableCell>
+      </TableRow>
+      <TableRow key='volumen total'>
+        <TableCell component="th" scope="row">
+          Volumen Total
+        </TableCell>
+        <TableCell align="right">{sellVolume + buyVolume}</TableCell>
+      </TableRow>
+      <TableRow key='cantidad acciones'>
+        <TableCell component="th" scope="row">
+          Cantidad de Acciones
+        </TableCell>
+        <TableCell align="right">{(exchangeStocks && exchangeStocks.length) || 0}</TableCell>
+      </TableRow>
+      <TableRow key='participacion de mercado'>
+        <TableCell component="th" scope="row">
+          Participación de Mercado
+        </TableCell>
+        <TableCell align="right">{(exchangeStocks && marketShare(exchange, exchanges, stocks, buy, sell, sellVolume + buyVolume)) || 0}%</TableCell>
+      </TableRow>
+    </Table>
+  )
+}
   
-  export default ExchangesTable;
+export default ExchangesTable;
   
